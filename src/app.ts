f30d1856f74051cdb5b3ca6e84a1ec984e6363d6
moises-herrera <moisesherrera037@gmail.com>
2025-03-04 20:13:02 -0500

    chore: add dependencies

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,8 @@ import { envConfig } from 'src/config/env-config';
 import { gitHubWebhooksMiddleware } from './github/webhooks-config';
 import { router } from './router';
 import { connectToDatabase } from './database/db-connection';
-import { exceptionMiddleware } from './common/middlewares/exception.middleware';
+import { handleHttpExceptionMiddleware } from './common/middlewares/handle-http-exception.middleware';
+import { sessionMiddleware } from './common/middlewares/session.middleware';
 
 const PORT = envConfig.PORT || 3000;
 
@@ -19,9 +20,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
+app.use(sessionMiddleware);
+
 app.use(router);
 
-app.use(exceptionMiddleware);
+app.use(handleHttpExceptionMiddleware);
 
 connectToDatabase();
 
