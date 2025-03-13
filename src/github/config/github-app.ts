cf4983cf9e779b11bfdf46bbdeace9f7a12b310c
moises-herrera <moisesherrera037@gmail.com>
2025-03-13 13:56:25 -0500

    refactor: read private key from .env file

--- src/github/config/github-app.ts
+++ src/github/config/github-app.ts
@@ -1,10 +1,9 @@
-import { readFileSync } from 'node:fs';
 import { App } from 'octokit';
 import { envConfig } from 'src/config/env-config';
 
 export const gitHubApp = new App({
   appId: envConfig.GITHUB_APP_ID,
-  privateKey: readFileSync(envConfig.GITHUB_PRIVATE_KEY_PATH, 'utf8'),
+  privateKey: envConfig.GITHUB_PRIVATE_KEY_PATH,
   webhooks: {
     secret: envConfig.GITHUB_WEBHOOK_SECRET,
   },
