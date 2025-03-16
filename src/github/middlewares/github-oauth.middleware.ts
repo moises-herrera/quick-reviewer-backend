8fafd51d594c2469b8528eeccb558b67591281c7
moises-herrera <moisesherrera037@gmail.com>
2025-03-15 20:39:55 -0500

    refactor: test log

--- src/github/middlewares/github-oauth.middleware.ts
+++ src/github/middlewares/github-oauth.middleware.ts
@@ -11,6 +11,8 @@ export const gitHubOAuthMiddleware = (
     const { code, state } = req.query;
     const { oauthState } = req.cookies;
 
+    console.log({ oauthState, code, state });
+
     if (!oauthState || state !== oauthState) {
       throw new HttpException('Invalid state parameter', StatusCodes.FORBIDDEN);
     }
