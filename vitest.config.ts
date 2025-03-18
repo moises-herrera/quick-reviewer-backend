04ac208166c1c791625d701732dcbf170298a8f5
moises-herrera <moisesherrera037@gmail.com>
2025-03-17 19:02:19 -0500

    chore: separate tests configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,23 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    environment: 'node',
-    globals: true,
+    workspace: [
+      {
+        test: {
+          name: 'unit',
+          include: ['tests/unit/**/*unit.test.ts'],
+          environment: 'node',
+          globals: true,
+        },
+      },
+      {
+        test: {
+          name: 'integration',
+          include: ['tests/integration/**/*integration.test.ts'],
+          environment: 'node',
+          globals: true,
+        },
+      },
+    ],
   },
 });
