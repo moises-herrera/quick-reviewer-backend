2944e41063da6877cddcd59a64b68e9e63b31e5a
moises-herrera <moisesherrera037@gmail.com>
2025-04-17 21:21:49 -0500

    chore: add project settings repository

---
+++ src/common/database/repositories/postgres-project-settings.repository.ts
@@ -0,0 +1,55 @@
+import { RepositorySettings } from '@prisma/client';
+import { inject, injectable } from 'inversify';
+import { ProjectSettingsRepository } from 'src/common/database/abstracts/project-settings.repository';
+import { DbClient } from 'src/common/database/db-client';
+import { BotSettings } from 'src/common/interfaces/bot-settings';
+
+@injectable()
+export class PostgresProjectSettingsRepository
+  implements ProjectSettingsRepository
+{
+  constructor(@inject(DbClient) private readonly dbClient: DbClient) {}
+
+  async getSettings(accountId: string): Promise<RepositorySettings[]> {
+    return this.dbClient.repositorySettings.findMany({
+      where: {
+        repository: {
+          ownerId: accountId,
+        },
+      },
+    });
+  }
+
+  async setSettings(projectId: string, settings: BotSettings): Promise<void> {
+    await this.dbClient.repositorySettings.upsert({
+      where: {
+        repositoryId: projectId,
+      },
+      create: {
+        repositoryId: projectId,
+        ...settings,
+      },
+      update: {
+        ...settings,
+      },
+    });
+  }
+
+  async deleteSettings(projectId: string): Promise<void> {
+    await this.dbClient.repositorySettings.deleteMany({
+      where: {
+        repositoryId: projectId,
+      },
+    });
+  }
+
+  async syncSettingsWithAccount(accountId: string): Promise<void> {
+    await this.dbClient.repositorySettings.deleteMany({
+      where: {
+        repository: {
+          ownerId: accountId,
+        },
+      },
+    });
+  }
+}
