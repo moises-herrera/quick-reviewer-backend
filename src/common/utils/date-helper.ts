059380b8627e5ee5c0a37787b91a492626e81663
moises-herrera <moisesherrera037@gmail.com>
2025-04-20 16:14:49 -0500

    fix: filter pull requests by date range

--- src/common/utils/date-helper.ts
+++ src/common/utils/date-helper.ts
@@ -3,3 +3,9 @@ export const changeMonths = (date: Date, months: number): Date => {
   newDate.setMonth(newDate.getMonth() + months);
   return newDate;
 };
+
+export const addDays = (date: Date, days: number): Date => {
+  const newDate = new Date(date);
+  newDate.setDate(newDate.getDate() + days);
+  return newDate;
+};
