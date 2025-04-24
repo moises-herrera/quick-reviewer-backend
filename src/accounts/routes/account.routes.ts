0fa4c132701bcad5ff744e2260dfa04ee89658c1
moises-herrera <moisesherrera037@gmail.com>
2025-04-23 20:40:52 -0500

    test: account routes

--- src/accounts/routes/account.routes.ts
+++ src/accounts/routes/account.routes.ts
@@ -10,7 +10,7 @@ import { gitHubAuthMiddleware } from 'src/github/middlewares/github-auth.middlew
  */
 const accountsRouter = Router();
 
-const registerRoutes = () => {
+export const registerRoutes = () => {
   accountsRouter.use(gitHubAuthMiddleware);
   accountsRouter.use(accountSettingsRouter);
 };
