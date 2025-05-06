8aa1cb2d20889aac3e4d95cd5741968d4472d540
moises-herrera <moisesherrera037@gmail.com>
2025-05-06 11:40:28 -0500

    chore: add middleware to restrict accounts

---
+++ src/github/middlewares/validate-github-account.middleware.ts
@@ -0,0 +1,63 @@
+import { NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { container } from 'src/app/config/container-config';
+import { envConfig } from 'src/app/config/env-config';
+import { TestAccountRepository } from 'src/common/database/abstracts/test-account.repository';
+
+const isAccountAuthorized = async (accountName: string): Promise<boolean> => {
+  const testAccountRepository = container.get(TestAccountRepository);
+  try {
+    const account =
+      await testAccountRepository.findTestAccountByName(accountName);
+    return !!account;
+  } catch (error) {
+    console.error(`Error validating account ${accountName}:`, error);
+    return false;
+  }
+};
+export const validateGitHubAccountMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  if (req.path !== envConfig.GITHUB_WEBHOOK_PATH) {
+    return next();
+  }
+
+  try {
+    const payload = req.body;
+    let accountName: string | null = null;
+
+    if (payload.installation && payload.installation.account) {
+      accountName = payload.installation.account.login;
+    } else if (payload.repository && payload.repository.owner) {
+      accountName = payload.repository.owner.login;
+    }
+
+    if (!accountName) {
+      console.warn(
+        'The account could not be identified:',
+        req.headers['x-github-event'],
+      );
+      return res.status(StatusCodes.FORBIDDEN).send('Account not identified');
+    }
+
+    const isAuthorized = await isAccountAuthorized(accountName);
+
+    if (!isAuthorized) {
+      console.warn(
+        `The account has not permissions to use this app: ${accountName}`,
+      );
+      return res
+        .status(StatusCodes.FORBIDDEN)
+        .send('The account does not have permissions to use this app');
+    }
+
+    next();
+  } catch (error) {
+    console.error('Error when validating account:', error);
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .send('Error when validating account');
+  }
+};
